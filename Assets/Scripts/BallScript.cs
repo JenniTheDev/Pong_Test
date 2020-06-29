b6aa715009f256a98156e6e2a6d0e48135d555fd
Jennifer Felton <justlittleoldmejenn@gmail.com>
2020-06-29 15:36:19 -0700

    Start
    Still need to limit paddle movement

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    [SerializeField] float forceValue = 4.5f;
+
+    Rigidbody2D myBody;
+    // Start is called before the first frame update
+    void Start() {
+        myBody = GetComponent<Rigidbody2D>();
+        myBody.AddForce(new Vector2(forceValue * 50, 50));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+    [SerializeField] bool isPlayerTwo;
+    [SerializeField] float speed = 0.2f; //how far paddle moves per frame
+    Transform myTransform;
+
+    // Start is called before the first frame update
+    void Start() {
+        myTransform = transform; // initialize 
+    }
+
+    // FixedUpdate is called once per physic tick
+    void FixedUpdate() {
+
+        // Is it player one or player two? 
+        if (isPlayerTwo) {
+            if (Input.GetKey("o"))
+                MoveUp();
+            else if (Input.GetKey("l"))
+                MoveDown();
+        }
+         else {
+            if (Input.GetKey("q"))
+                MoveUp();
+            else if (Input.GetKey("a"))
+                MoveDown();
+        }
+    }
+
+    void MoveUp() {
+        myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y + speed);
+    }
+
+    void MoveDown() {
+        myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y - speed);
+    }
+
+
+}
